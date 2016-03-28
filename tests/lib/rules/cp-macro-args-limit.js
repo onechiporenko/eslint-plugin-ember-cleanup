16c2f0ce4dfa4fcc5c5fa118563378fe61a478be
Oleg Nechiporenko <onechiporenko@apache.org>
2016-03-28 12:07:51 +0300

    Update rule `cp-macro-args-limit` to support eq/min/max number for each macro separately

--- tests/lib/rules/cp-macro-args-limit.js
+++ tests/lib/rules/cp-macro-args-limit.js
@@ -19,9 +19,11 @@ var j = new Jsonium();
 // Tests
 //------------------------------------------------------------------------------
 
-var m = "`computed.{{MACRO}}` is called with {{NUM}} dependent key(s). Minimum number should be {{MIN}}";
+var mAtLeast = "`computed.{{MACRO}}` is called with {{NUM}} dependent key(s). Must be at least {{MIN}} dependent key(s)";
+var mAtMost = "`computed.{{MACRO}}` is called with {{NUM}} dependent key(s). Must be at most {{MAX}} dependent key(s)";
+var mEq = "`computed.{{MACRO}}` is called with {{NUM}} dependent key(s). Must be only {{EQ}} dependent key(s)";
 
-var macros = [
+var macrosForMin = [
   {MACRO: "and"},
   {MACRO: "or"}
 ];
@@ -30,102 +32,277 @@ var customMacro = [{MACRO: "customMacro"}];
 
 var codes = require("./keys.js").macro;
 
-var validKeys = [
+var validKeysForMin = [
   {KEYS: "'a', 'b', 'c'", MIN: 2},
   {KEYS: "'a', 'b', 'c', 'd'", MIN: 3},
   {KEYS: "'a', 'b', 'c', 'd', 'e'", MIN: 4}
 ];
 
-var invalidKeys = [
+var invalidKeysForMin = [
   {KEYS: "'a'", NUM: 1, MIN: 2},
   {KEYS: "'a' + val", NUM: 1, MIN: 2},
   {KEYS: "'a', 'b'", NUM: 2, MIN: 3},
   {KEYS: "'a', 'b', 'c'", NUM: 3, MIN: 4}
 ];
 
-var validTestTemplates = [
+var validTestTemplatesForMin = [
   {
     code:
       "{{CODE}}",
     options: [{
-      min: "{{MIN}}"
+      customMacro: {min: "{{MIN}}"},
+      and: {min: "{{MIN}}"},
+      or: {min: "{{MIN}}"}
     }]
   }
 ];
 
-var invalidTestTemplates = [
+var invalidTestTemplatesForMin = [
   {
     code:
       "{{CODE}}",
     options: [{
-      min: "{{MIN}}"
+      customMacro: {min: "{{MIN}}"},
+      and: {min: "{{MIN}}"},
+      or: {min: "{{MIN}}"}
     }],
     errors: [
-      {message: m, type: "CallExpression"}
+      {message: mAtLeast, type: "CallExpression"}
     ]
   }
 ];
 
 
-function parseMin(test) {
-  if (test.options[0].hasOwnProperty("min")) {
-    test.options[0].min = parseInt(test.options[0].min, 10);
-  }
-  return test;
-}
-
-function addCustomMacro(test) {
-  test.options[0].check = [customMacro[0].MACRO];
+function parseCount(test) {
+  Object.keys(test.options[0]).forEach(function (k) {
+    ["min", "max", "eq"].forEach(function (p) {
+      if (test.options[0][k].hasOwnProperty(p)) {
+        test.options[0][k][p] = parseInt(test.options[0][k][p], 10);
+      }
+    });
+  });
   return test;
 }
 
 var validTests = j
-  .setTemplates(validTestTemplates)
+  .setTemplates(validTestTemplatesForMin)
   .createCombos(["code"], codes)
   .useCombosAsTemplates()
-  .createCombos(["code"], macros)
+  .createCombos(["code"], macrosForMin)
   .useCombosAsTemplates()
-  .createCombos(["code", "options.0.min"], validKeys)
+  .createCombos(["code", "options.0.{and,or}.min"], validKeysForMin)
   .uniqueCombos()
   .getCombos();
 
 var customValidTests = j
-  .setTemplates(validTestTemplates)
+  .setTemplates(validTestTemplatesForMin)
   .createCombos(["code"], codes)
   .useCombosAsTemplates()
   .createCombos(["code"], customMacro)
   .useCombosAsTemplates()
-  .createCombos(["code", "options.0.min"], validKeys)
+  .createCombos(["code", "options.0.{and,or,customMacro}.min"], validKeysForMin)
   .uniqueCombos()
-  .getCombos()
-  .map(addCustomMacro);
+  .getCombos();
 
-var allValidTests = validTests.concat(customValidTests).map(parseMin);
+var allValidTests = validTests.concat(customValidTests).map(parseCount);
 
 var invalidTests = j
-  .setTemplates(invalidTestTemplates)
+  .setTemplates(invalidTestTemplatesForMin)
   .createCombos(["code"], codes)
   .useCombosAsTemplates()
-  .createCombos(["code", "errors.0.message"], macros)
+  .createCombos(["code", "errors.0.message"], macrosForMin)
   .useCombosAsTemplates()
-  .createCombos(["code", "options.0.min", "errors.0.message"], invalidKeys)
+  .createCombos(["code", "options.0.{and,or}.min", "errors.0.message"], invalidKeysForMin)
   .uniqueCombos()
   .getCombos();
 
 var customInvalidTests = j
-  .setTemplates(invalidTestTemplates)
+  .setTemplates(invalidTestTemplatesForMin)
   .createCombos(["code"], codes)
   .useCombosAsTemplates()
   .createCombos(["code", "errors.0.message"], customMacro)
   .useCombosAsTemplates()
-  .createCombos(["code", "options.0.min", "errors.0.message"], invalidKeys)
+  .createCombos(["code", "options.0.{and,or,customMacro}.min", "errors.0.message"], invalidKeysForMin)
   .uniqueCombos()
-  .getCombos()
-  .map(addCustomMacro);
+  .getCombos();
 
-var allInvalidTests = invalidTests.concat(customInvalidTests).map(parseMin);
+var allInvalidTests = invalidTests.concat(customInvalidTests).map(parseCount);
 
 var ruleTester = new RuleTester({env: {es6: true}});
+ruleTester.run("cp-macro-args-limit", rule, {
+  valid: allValidTests,
+  invalid: allInvalidTests
+});
+
+/* EQ */
+
+var macrosForEq = [
+  {MACRO: "max"},
+  {MACRO: "min"}
+];
+
+var validTestTemplatesForEq = [
+  {
+    code:
+      "{{CODE}}",
+    options: [{
+      customMacro: {eq: "{{EQ}}"},
+      min: {eq: "{{EQ}}"},
+      max: {eq: "{{EQ}}"}
+    }]
+  }
+];
+
+var invalidTestTemplatesForEq = [
+  {
+    code:
+      "{{CODE}}",
+    options: [{
+      customMacro: {eq: "{{EQ}}"},
+      min: {eq: "{{EQ}}"},
+      max: {eq: "{{EQ}}"}
+    }],
+    errors: [
+      {message: mEq, type: "CallExpression"}
+    ]
+  }
+];
+
+var validKeysForEq = [
+  {KEYS: "'a', 'b'", EQ: 2},
+  {KEYS: "'a', 'b', 'c'", EQ: 3},
+  {KEYS: "'a', 'b', 'c', 'd'", EQ: 4}
+];
+
+var invalidKeysForEq = [
+  {KEYS: "'a'", NUM: 1, EQ: 2},
+  {KEYS: "'a', 'b', 'c'", NUM: 3, EQ: 2},
+  {KEYS: "'a' + val", NUM: 1, EQ: 2},
+  {KEYS: "'a', 'b'", NUM: 2, EQ: 3},
+  {KEYS: "'a', 'b', 'c', 'd'", NUM: 4, EQ: 3},
+  {KEYS: "'a', 'b', 'c'", NUM: 3, EQ: 4}
+];
+
+validTests = j
+  .setTemplates(validTestTemplatesForEq)
+  .createCombos(["code"], codes)
+  .useCombosAsTemplates()
+  .createCombos(["code"], macrosForEq)
+  .useCombosAsTemplates()
+  .createCombos(["code", "options.0.{max,min}.eq"], validKeysForEq)
+  .uniqueCombos()
+  .getCombos();
+
+customValidTests = j
+  .setTemplates(validTestTemplatesForEq)
+  .createCombos(["code"], codes)
+  .useCombosAsTemplates()
+  .createCombos(["code"], customMacro)
+  .useCombosAsTemplates()
+  .createCombos(["code", "options.0.{max,min,customMacro}.eq"], validKeysForEq)
+  .uniqueCombos()
+  .getCombos();
+
+allValidTests = validTests.concat(customValidTests).map(parseCount);
+
+invalidTests = j
+  .setTemplates(invalidTestTemplatesForEq)
+  .createCombos(["code"], codes)
+  .useCombosAsTemplates()
+  .createCombos(["code", "errors.0.message"], macrosForEq)
+  .useCombosAsTemplates()
+  .createCombos(["code", "options.0.{max,min}.eq", "errors.0.message"], invalidKeysForEq)
+  .uniqueCombos()
+  .getCombos();
+
+customInvalidTests = j
+  .setTemplates(invalidTestTemplatesForEq)
+  .createCombos(["code"], codes)
+  .useCombosAsTemplates()
+  .createCombos(["code", "errors.0.message"], customMacro)
+  .useCombosAsTemplates()
+  .createCombos(["code", "options.0.{max,min,customMacro}.eq", "errors.0.message"], invalidKeysForEq)
+  .uniqueCombos()
+  .getCombos();
+
+allInvalidTests = invalidTests.concat(customInvalidTests).map(parseCount);
+
+ruleTester.run("cp-macro-args-limit", rule, {
+  valid: allValidTests,
+  invalid: allInvalidTests
+});
+
+/* MAX */
+
+var macrosForMax = [
+  {MACRO: "superMacro1"},
+  {MACRO: "superMacro2"}
+];
+
+var validTestTemplatesForMax = [
+  {
+    code:
+      "{{CODE}}",
+    options: [{
+      superMacro1: {max: "{{MAX}}"},
+      superMacro2: {max: "{{MAX}}"}
+    }]
+  }
+];
+
+var invalidTestTemplatesForMax = [
+  {
+    code:
+      "{{CODE}}",
+    options: [{
+      superMacro1: {max: "{{MAX}}"},
+      superMacro2: {max: "{{MAX}}"}
+    }],
+    errors: [
+      {message: mAtMost, type: "CallExpression"}
+    ]
+  }
+];
+
+var validKeysForMax = [
+  {KEYS: "'a'", MAX: 2},
+  {KEYS: "'a', 'b'", MAX: 2},
+  {KEYS: "'a', 'b'", MAX: 3},
+  {KEYS: "'a', 'b', 'c'", MAX: 3},
+  {KEYS: "'a', 'b', 'c'", MAX: 4},
+  {KEYS: "'a', 'b', 'c', 'd'", MAX: 4}
+];
+
+var invalidKeysForMax = [
+  {KEYS: "'a', 'b', 'c'", NUM: 3, MAX: 2},
+  {KEYS: "'a', 'b'", NUM: 2, MAX: 1},
+  {KEYS: "'a', 'b', 'c', 'd'", NUM: 4, MAX: 3}
+];
+
+validTests = j
+  .setTemplates(validTestTemplatesForMax)
+  .createCombos(["code"], codes)
+  .useCombosAsTemplates()
+  .createCombos(["code"], macrosForEq)
+  .useCombosAsTemplates()
+  .createCombos(["code", "options.0.{superMacro1,superMacro2}.max"], validKeysForMax)
+  .uniqueCombos()
+  .getCombos();
+
+allValidTests = validTests.map(parseCount);
+
+invalidTests = j
+  .setTemplates(invalidTestTemplatesForMax)
+  .createCombos(["code"], codes)
+  .useCombosAsTemplates()
+  .createCombos(["code", "errors.0.message"], macrosForMax)
+  .useCombosAsTemplates()
+  .createCombos(["code", "options.0.{superMacro1,superMacro2}.max", "errors.0.message"], invalidKeysForMax)
+  .uniqueCombos()
+  .getCombos();
+
+allInvalidTests = invalidTests.map(parseCount);
+
 ruleTester.run("cp-macro-args-limit", rule, {
   valid: allValidTests,
   invalid: allInvalidTests
